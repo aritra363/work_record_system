7e7f5044507be8a535b22d39b2f3ebf2a6452726
aritra363 <30936415+aritra363@users.noreply.github.com>
2020-07-27 13:34:03 +0530

    Add files via upload

---
+++ JS/fetch.js
@@ -0,0 +1,121 @@
+function fetchData() {
+    let table = document.querySelector('.table');
+    let home = document.querySelector('#home');
+    home.disabled = true;
+    let url = 'backend/fetchData.php?fetch=1';
+    let spinner = document.querySelector('.spinner');
+    table.innerHTML += `<tbody id="data"></tbody>`;
+    let table_data = document.querySelector('#data');
+    fetch(url).then(res => res.json())
+    .then (data => {
+        if (data.length) {
+            data.forEach((item,index) => {
+                table_data.innerHTML += `
+                    <tr>
+                      <td>${index+1}</td>
+                      <td> <input type="text" id="name${+item['id']}" class="form-control"  value="${item['Name']}"></td>
+                      <td> <input type="text" id="work_type${+item['id']}" class="form-control"  value="${item['Work_Type']}"></td>
+                      <td> <input type="date"id="e_date${+item['id']}" class="form-control"  value="${item['End_Date']}"></td>
+                      <td>Rs.<input type="number"id="pd${+item['id']}" class="form-control"  value="${item['Payment_dn']}"></td>
+                      <td>Rs.<input type="number"id="pl${+item['id']}" class="form-control"  value="${item['Payment_lt']}"></td>
+                      <td><button type="button" onclick="edit(${item['id']})" class="btn btn-info">Edit</button>&nbsp;&nbsp;<button type="button" onclick="del(${item['id']})" class="btn btn-info">Delete</button></td>
+                    </tr>`;
+            });
+        } else {
+            table.innerHTML += `<tbody><tr><td colspan='7' style='text-align:center;'>No records found</td><tr></tbody>`;
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        table.innerHTML += `<tbody><tr><td colspan='7' style='text-align:center;'>No records found</td><tr></tbody>`;
+    })
+}
+function load_data() {
+    let table = document.querySelector('.table');
+    let search_bar = document.querySelector('.search-bar');
+    let table_data = document.querySelector('#data');
+    let add_form = document.querySelector('#add-form');
+    table_data.remove();
+    fetchData();
+    table.style.display="block";
+    search_bar.style.display = 'block';
+    if (!(add_form === null)) {
+        add_form.style.display = "none";
+    }
+}
+function add_data() {
+    let table = document.querySelector('.table');
+    let search_bar = document.querySelector('.search-bar');
+    let add_form = document.querySelector('#add-form');
+    if (!(add_form === null)) {
+        add_form.remove();
+    }
+    search_bar.style.display = 'none';
+    table.style.display="none";  
+    let container = document.querySelector('.container-fluid');
+    container.innerHTML += `<form id='add-form'>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Name</label>
+      <input type="text" class="form-control" id="name" aria-describedby="emailHelp" required>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Work Type</label>
+      <input type="text" class="form-control" id="work_type">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">End Date</label>
+      <input type="date" class="form-control" id="e_date">
+    </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Payment Done</label>
+    <input type="number" class="form-control" id="pd">
+    </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Payment Left</label>
+    <input type="number" class="form-control" id="pl">
+    </div>
+    <button id="submit" class="btn btn-primary">Submit</button>
+    </form>`; 
+    let submit_btn = document.querySelector('#submit');
+    let name = document.querySelector('#name');
+    let work_type = document.querySelector('#work_type');
+    let e_date = document.querySelector('#e_date');
+    let pd = document.querySelector('#pd');
+    let pl = document.querySelector('#pl');
+    submit_btn.addEventListener('click',function (e){
+        e.preventDefault();
+        if (name.value.trim() !== '') {
+            let url = 'backend/fetchData.php?add=1&name='+name.value.replace(' ','%20')+'&work_type='+work_type.value.replace(' ','%20')+'&e_date='+e_date.value+'&pd='+pd.value+'&pl='+pl.value;
+            fetch(url).then(res => res.json())
+            .then (data => {
+                if (data == 1) {
+                    alert ('Data inserted Succesfully');
+                } else {
+                    alert ('something went wrong!');
+                }
+                load_data();
+            })
+            
+        }
+    });
+}
+function del(id) {
+    let url = 'backend/fetchData.php?delete='+id;
+    fetch (url).then(res => {
+        load_data();
+    });
+}
+function edit(id) {
+    let name = document.querySelector('#name'+id);
+    let work_type = document.querySelector('#work_type'+id);
+    let e_date = document.querySelector('#e_date'+id);
+    let pd = document.querySelector('#pd'+id);
+    let pl = document.querySelector('#pl'+id);
+    let url = 'backend/fetchData.php?edit='+id+'&name='+name.value.replace(' ','%20')+'&work_type='+work_type.value.replace(' ','%20')+'&e_date='+e_date.value+'&pd='+pd.value+'&pl='+pl.value;
+    fetch (url).then(res => {
+        load_data();
+    });
+}
+function search(value) {
+ console.log(value)
+}
---
+++ backend/fetchData.php
@@ -0,0 +1,42 @@
+<?php
+    $dbConnection = mysqli_connect("localhost","root","","work_record");
+    if (!$dbConnection) {
+        die();
+    } else {
+        if (isset($_GET['fetch']) && $_GET['fetch'] == 1) {
+            $fetchData = "select * from works";
+            $run = mysqli_query($dbConnection,$fetchData);
+            $getData = mysqli_fetch_all($run,MYSQLI_ASSOC);
+            echo json_encode($getData);
+            die();
+        } 
+        if (isset($_GET['add']) && $_GET['add'] == 1) {
+            $name = $_GET['name'];
+            $wt = $_GET['work_type'] == '' ? 'Nil' : $_GET['work_type'] ;
+            $ed = $_GET['e_date']== '' ? 'Nil' : $_GET['e_date'] ;
+            $pd = $_GET['pd'] == '' ? 'Nil' : $_GET['pd'] ;;
+            $pl = $_GET['pl']== '' ? 'Nil' : $_GET['pl'] ;;
+            $insert_data = "insert into works(Name,Work_Type,End_Date,Payment_dn,Payment_lt) VALUES ('".$name."','".$wt."','".$ed."','".$pd."','".$pl."')";
+            $run = mysqli_query($dbConnection,$insert_data);
+            if ($run) {
+                echo "1";
+            } else {
+                echo "0";
+            }
+        }
+        if (isset($_GET['delete'])) {
+            $delete_data = 'delete from works where id='.$_GET['delete'];
+            $run = mysqli_query($dbConnection,$delete_data);
+        }
+        if (isset($_GET['edit'])) {
+            $name = $_GET['name'] == '' ?'Nil' : $_GET['name'];
+            $wt = $_GET['work_type'] == '' ? 'Nil' : $_GET['work_type'] ;
+            $ed = $_GET['e_date']== '' ? 'Nil' : $_GET['e_date'] ;
+            $pd = $_GET['pd'] == '' ? 'Nil' : $_GET['pd'] ;;
+            $pl = $_GET['pl']== '' ? 'Nil' : $_GET['pl'] ;;
+            $edit_data = "update works set Name = '".$name."',Work_Type='".$wt."',End_Date='".$ed."',Payment_dn='".$pd."',Payment_lt='".$pl."' where id =".$_GET['edit'];
+            $run = mysqli_query($dbConnection,$edit_data);
+        }
+        die();
+    }
+?>
